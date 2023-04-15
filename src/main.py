3ac6efd56f82b9f25348d113bab550d3f7239fa7
Dar3cz3Q <45432804+Dar3cz3Q@users.noreply.github.com>
2023-04-15 23:19:06 +0200

    Added CORS exception

--- src/main.py
+++ src/main.py
@@ -3,11 +3,24 @@ from src.routes.home import home
 from src.routes.employees import employees
 from src.routes.items import items
 from src.routes.tasks import tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3001'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(home.router)
 app.include_router(employees.router)
