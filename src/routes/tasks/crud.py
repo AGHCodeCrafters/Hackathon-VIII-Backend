62675ae053dbeb24f9222ed7cea6b955cafb9dc4
Kamil Kordecki <kkordecky@gmail.com>
2023-04-15 21:53:38 +0200

    feat: Add read_tasks_by_employee

--- src/routes/tasks/crud.py
+++ src/routes/tasks/crud.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from fastapi import HTTPException
 
+from typing import List
+
 def create_task(db: Session, task: schemas.TaskBase):
     db_task = models.Task(**task.dict())
     db.add(db_task)
@@ -14,6 +16,13 @@ def get_task(db: Session, task_id: int):
 
 def get_tasks(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Task).offset(skip).limit(limit).all()
+
+def get_tasks_by_employee_id(db: Session, employee_id: int) -> List[models.Task]:
+    tasks = db.query(models.Task).filter(models.Task.employee_id == employee_id).all()
+    if not tasks:
+        raise HTTPException(status_code=404, detail="Tasks not found")
+    return tasks
+
 
 def complete_task(db: Session, task_id: int):
     db_task = db.query(models.Task).filter(models.Task.id == task_id, models.Task.status != "COMPLETED").first()
@@ -33,3 +42,5 @@ def complete_task(db: Session, task_id: int):
     db_task.status = "COMPLETED"
     db.commit()
     return {"message": "Task completed successfully"}
+
+
--- src/routes/tasks/tasks.py
+++ src/routes/tasks/tasks.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from . import crud, models, schemas
+from ..employees.crud import get_employee
 from ...config import database  # import SessionLocal, engine
 
 database.Base.metadata.create_all(bind=database.engine)
@@ -33,6 +34,14 @@ def read_task(task_id: int, db: Session = Depends(get_db)):
 def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     tasks = crud.get_tasks(db=db, skip=skip, limit=limit)
     return tasks
+
+@router.get("/tasks/employee/{employee_id}", response_model=List[schemas.Task])
+def read_tasks_by_employee(employee_id: int, db: Session = Depends(get_db)):
+    db_employee = get_employee(db=db, employee_id=employee_id)
+    if db_employee is None:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return crud.get_tasks_by_employee_id(db=db, employee_id=employee_id)
+
 
 @router.patch("/tasks/{task_id}/complete")
 def complete_task(task_id: int, db: Session = Depends(get_db)):
